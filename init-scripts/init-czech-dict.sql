61873e238b512c7de25ec447407e29b1628107f9
Ondřej Musil <omusil@gmail.com>
2018-04-13 09:11:31 +0200

    fix: Init script with correct file names

--- init-scripts/init-czech-dict.sql
+++ init-scripts/init-czech-dict.sql
@@ -1,5 +1,5 @@
 CREATE TEXT SEARCH DICTIONARY cspell
-   (template=ispell, dictfile = czech, afffile=czech, stopwords=czech);
+   (template=ispell, dictfile = czech_unaccent, afffile=czech_unaccent, stopwords=czech_unaccent);
 CREATE TEXT SEARCH CONFIGURATION cs (copy=english);
 ALTER TEXT SEARCH CONFIGURATION cs
   ALTER MAPPING FOR word, asciiword WITH cspell, simple;
